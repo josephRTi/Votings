21779dce51910f33178e64f6609c7ceb311394dc
josephRTi <52317049+josephRTi@users.noreply.github.com>
2021-06-22 23:51:59 +0300

    Update serializers.py

--- simpleVotings/serializers.py
+++ simpleVotings/serializers.py
@@ -2,14 +2,6 @@ from rest_framework import serializers
 from .models import *
 
 
-class CurrentUserDefault(object):
-    def set_context(self, serializer_field):
-        self.user_id = serializer_field.context['request'].user.id
-
-    def __call__(self):
-        return self.user_id
-
-
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
